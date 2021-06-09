f67add97a6532cd0229012a9ff9710536b5e3422
Jonathas <jonathas-25@hotmail.com>
2021-06-09 19:14:23 -0300

    #4 Subindo o Bootstrap
    Aqui fizemos o donwload do bootstrap e colocamos no projeto (para quem fez a aula de páginas html com bootstrap é só copiar e colar a pasta de um projeto para o  outro).
    
    Colocado um grid para melhorar a aparência.

--- js/main.js
+++ js/main.js
@@ -15,9 +15,9 @@ function consultaCep () { /* função para puxar o cep*/
             console.log(response);
             $("#logradouro").html(response.logradouro); /*Mesma coisa do que document.getElementById porem com menos código.*/
             //document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("bairro").innerHTML = response.bairro;
-            document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("uf").innerHTML = response.uf;
+            //document.getElementById("bairro").innerHTML = response.bairro;
+            //document.getElementById("localidade").innerHTML = response.localidade;
+            //document.getElementById("uf").innerHTML = response.uf;
            //alert(response.logradouro);
         } 
 
