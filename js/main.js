f6a63329c82a7035aa79dad8b5d51d4ea9027ba5
Jonathas <jonathas-25@hotmail.com>
2021-06-09 11:57:12 -0300

    #3 Começando a aula
    Começando a fazer as funções da aula.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-alert("Funcionou?");
+function consultaCep () { /* função para puxar o cep*/
+    $.ajax({ /*Função já no ajax*/
+        url: "http://viacep.com.br/ws/25520171/json/",
+        type: "GET",
+        success: function(response){
+            console.log(response);
+        } 
+
+    })
+
+  
+
+}
