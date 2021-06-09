dfeeddd9452874a63e4d10585339c8e2e29085e8
Jonathas <jonathas-25@hotmail.com>
2021-06-09 12:17:17 -0300

    #3 Puxando o CEP
    Criado uma função para puxar qualquer cep, depois vamos colocar todas as informações na tela.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 function consultaCep () { /* função para puxar o cep*/
+
+
+
+    var cep = document.getElementById("cep").value; /*Pega o valor que o user digitar*/
+    var url = "http://viacep.com.br/ws/" + cep + "/json/"; /*Já pega o cep de qualquer lugar*/
+
+    console.log(url);
+
+
     $.ajax({ /*Função já no ajax*/
-        url: "http://viacep.com.br/ws/25520171/json/",
+        url: url,
         type: "GET",
         success: function(response){
             console.log(response);
+           //alert(response.logradouro);
         } 
 
     })
